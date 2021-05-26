249118afe01c60c998dbb01f636a20c53e3685d5
ChathuraLiyanapathirana <cbuddhika3@gmail.com>
2021-05-26 09:53:05 +0530

    Project Structore and fb config

---
+++ src/config/config.ts
@@ -0,0 +1,11 @@
+export const config = {
+    // DO NOT USE THESE CREDENTIALS ! THEY ARE HERE TO HELP IN THE LEARNING PROCESS.
+    // ANY AND ALL DATA ON THAT DOMAIN IS SUBJECT TO CHANGE AND REMOVAL AT ANY TIME
+    // THIS ACCOUNT IS ALSO ON THE FREE PLAN AND IS SUBJECT TO RESTRICTIONS !
+    apiKey: "AIzaSyAvJHkkfXn7h4RXpsYbATRSo6sbLMRPyd4",
+    authDomain: "react-fb-auth-9a2ce.firebaseapp.com",
+    projectId: "react-fb-auth-9a2ce",
+    storageBucket: "react-fb-auth-9a2ce.appspot.com",
+    messagingSenderId: "447010631045",
+    appId: "1:447010631045:web:f44fbe47a45517fd7228ec"
+  };
